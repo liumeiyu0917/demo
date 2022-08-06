2a08297e90f06ad8fbd0f9e9bc3f759305a03d1f
liumeiyu <liumeiyu0917@163.com>
2022-08-06 23:13:29 +0800

    v1.0

---
+++ sort/三个输入参数的排序.cpp
@@ -0,0 +1,37 @@
+# include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	int t;
+	printf("请输入三个数，并以空格分割\n");
+	
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if (b > a)
+	{
+		t = b;
+		b = a;
+		a = t;
+	}
+	
+	if (c > a)
+	{
+		t = c;
+		c = a;
+		a = t;		
+	}
+	
+	if (c > b)
+	{
+		t = c;
+		c = b;
+		b = t;			
+		
+	} 
+	
+	printf("从大到小排序为：%d %d %d\n", a, b, c);
+	
+	return 0;
+	
+}
